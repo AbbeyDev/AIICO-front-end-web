1ec788b000cca99786c277f59bb61bb8eed1958a
AbbeyDev <51371359+AbbeyDev@users.noreply.github.com>
2019-06-04 14:34:17 +0100

    Add files via upload

---
+++ AIICO/js/script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     var floating = $(".stay");
+//     var floatingPosition = floating.position;
+//     $(window).scroll(function(){
+//         var scrollBarPosition = $(window).scrollTop();
+//         if (scrollBarPosition >= floatingPosition.top){
+//             floating.css({
+//                 'position' : 'fixed',
+//                 'top' : 5
+//             }); 
+//         }
+//         else{
+//             floating.css({
+//                 'position' : 'relative',
+//                 'top' : 0
+//             });
+//         }
+//     });
+// })
